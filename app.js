9fc514c9141dd8b5cb0fdecb6ace14479bac53e5
ShaharShi <Shaharshitrit01@gmail.com>
2020-07-23 17:30:03 +0300

    Gal's rejects fixed

--- app.js
+++ app.js
@@ -6,43 +6,72 @@ function getUsers(params) {
   });
 }
 
-const mappingWithFunction = {
-  fullName: { fn: getName, isVisible: true },
-  country: { fn: getCountry, isVisible: true },
-  city: { fn: getCity, isVisible: true },
-  address: { fn: getAdress, isVisible: true },
-  src: { fn: getUserImgUrl, isVisible: true },
+// const mappingWithFunction = {
+//   fullName: { fn: getName, isVisible: true },
+//   country: { fn: getCountry, isVisible: true },
+//   city: { fn: getCity, isVisible: true },
+//   address: { fn: getAdress, isVisible: true },
+//   src: { fn: getUserImgUrl, isVisible: true },
+// };
+const mapping = {
+  firstName: { path: "name.first", isVisible: true },
+  lastName: { path: "name.last", isVisible: true },
+  country: { path: "location.country", isVisible: true },
+  city: { path: "location.city", isVisible: true },
+  address: { path: "location.street.name", isVisible: true },
+  src: { path: "picture.large", isVisible: true },
 };
 
 $(function () {
+  let numberOfUsersRequested;
+  const numberOfUsersValues = ["10", "20", "30", "40", "50"];
   const cardsContainer = $("#container");
   const numOfUserInLS = localStorage.getItem("numOfUsers");
-  if (numOfUserInLS) init(numOfUserInLS, cardsContainer);
+
+  drawNumberOfValues(numberOfUsersValues);
+
+  if (numOfUserInLS) {
+    $("#inputGroupSelect01").val(numOfUserInLS);
+    getData(numOfUserInLS, cardsContainer);
+  }
 
   $("#usersBtn").on("click", () => {
-    const numberOfUsers = $("#inputGroupSelect01 option:selected").val();
-    localStorage.setItem("numOfUsers", numberOfUsers);
-    init(localStorage.getItem("numOfUsers"), cardsContainer);
+    numberOfUsersRequested = $("#inputGroupSelect01 option:selected").val();
+    localStorage.setItem("numOfUsers", numberOfUsersRequested);
+    getData(localStorage.getItem("numOfUsers"), cardsContainer);
   });
 });
 
-function getName(user) {
-  return `${user.name.first} ${user.name.last}`;
-}
-function getCountry(user) {
-  return user.location.country;
-}
-function getCity(user) {
-  return user.location.city;
-}
-function getAdress(user) {
-  return `${user.location.street.name} ${user.location.street.number}`;
-}
-function getUserImgUrl(user) {
-  return user.picture.large;
-}
+// function getName(user) {
+//   const pathArr = ["name.first", "name.last"];
+//   const requestedVal = pathArr.map( path => { return checkVal(path, user) })
+//   return requestedVal.toString().replace(/,/, ' ')
+// }
+// function getCountry(user) {
+//   const pathArr = ["location.country"];
+//   const requestedVal = pathArr.map( path => { return checkVal(path, user) })
+//   return requestedVal.toString().replace(/,/, ' ')
+// }
+// function getCity(user) {
+//   return user.location.city;
+// }
+// function getAdress(user) {
+//   return `${user.location.street.name} ${user.location.street.number}`;
+// }
+// function getUserImgUrl(user) {
+//   return user.picture.large;
+// }
 
-async function init(numberOfUsers, cardsContainer) {
+// function checkVal(currentPath, user) {
+//   if (typeof currentPath !== "string") return
+//   const splittedPath = currentPath.split('.')
+//   return splittedPath.reduce((currentUser, partOfPath) => {
+//     const thereIsVal = currentUser[partOfPath]
+//     return thereIsVal ? currentUser[partOfPath]: 'Not Availble'
+//   }, user)
+// }
+
+async function getData(numberOfUsers, cardsContainer) {
   try {
     const response = await getUsers({
       url: `https://randomuser.me/api/?results=${numberOfUsers}`,
@@ -51,7 +80,7 @@ async function init(numberOfUsers, cardsContainer) {
     console.log(results);
     draw(results, cardsContainer);
   } catch (err) {
-    console.log(err);
+    console.error(err);
     console.error(`message: ${err.statusText} , status: ${err.status}`);
   }
 }
@@ -71,7 +100,7 @@ function draw(arrOfObjects, cardsContainer) {
 }
 
 function getMappedUser(user) {
-  const keyValueMappingArray = Object.entries(mappingWithFunction);
+  const keyValueMappingArray = Object.entries(mapping);
 
   return keyValueMappingArray.reduce((mappedUser, KEYVALUEPAIR_ARRAY) => {
     const [key, settingObj] = KEYVALUEPAIR_ARRAY;
@@ -84,12 +113,22 @@ function getMappedUser(user) {
   }, {});
 }
 
+function getValueFromPath(path, user) {
+  if (typeof path !== "string") return;
+  const splittedPath = path.split(".");
+  const theRequestedValue = splittedPath.reduce((currentUser, partOfPath) => {
+    const isValueExist = currentUser[partOfPath];
+    return isValueExist ? currentUser[partOfPath] : "Not Available";
+  }, user);
+  return theRequestedValue;
+}
+
 function getCardItem(user) {
   const cardWrap = $("<div></div>").attr("class", "card-wrap");
   const cardHead = $("<div></div>").attr("class", "card-head bg-light");
   const userImg = $(`<img src="${user.src}">`).attr("class", "card-user-img");
   const cardBody = $("<div></div>").attr("class", "card-body");
-  const userName = $("<h5></h5>").text(user.fullName);
+  const userName = $("<h5></h5>").html(`${user.firstName} ${user.lastName}`);
   const userCountry = $("<p>></p>").text(user.country);
   const userAdress = $("<p></p>").html(`${user.city} <br> ${user.address}`);
 
@@ -98,4 +137,13 @@ function getCardItem(user) {
   cardWrap.append(cardHead, cardBody);
 
   return cardWrap;
+}
+
+function drawNumberOfValues(numberOfUsersArr) {
+  const options = numberOfUsersArr.map((val) => {
+    const option = $(`<option value='${val}'>${val} Users </option>`);
+    return option;
+  });
+
+  $("#inputGroupSelect01").append(...options);
 }
